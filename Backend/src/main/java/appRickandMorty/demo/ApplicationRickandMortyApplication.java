6943f7f872c42af484304c402dc52b3c9e2d3b7f
yulian david <julianolomanolox@hotmail.com>
2022-10-09 22:04:17 -0500

    jum

--- Backend/src/main/java/appRickandMorty/demo/ApplicationRickandMortyApplication.java
+++ Backend/src/main/java/appRickandMorty/demo/ApplicationRickandMortyApplication.java
@@ -2,7 +2,6 @@ package appRickandMorty.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ApplicationRickandMortyApplication {
 
--- Backend/src/test/java/appRickandMorty/demo/ApplicationRickandMortyApplicationTests.java
+++ Backend/src/test/java/appRickandMorty/demo/ApplicationRickandMortyApplicationTests.java
@@ -3,11 +3,16 @@ package appRickandMorty.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
 @SpringBootTest
 class ApplicationRickandMortyApplicationTests {
-
 	@Test
 	void contextLoads() {
+		String someone = "World";
+
+		assertThat(someone).isEqualTo(someone);
 	}
 
 }
