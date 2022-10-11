5424e47904e358ad72c6cad5877e770f5c2b3715
yulian david <julianolomanolox@hotmail.com>
2022-10-10 21:27:34 -0500

    prueba

---
+++ Backend/src/test/java/appRickandMorty/demo/controller/ControllerRMTest.java
@@ -0,0 +1,33 @@
+package appRickandMorty.demo.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerRMTest {
+
+    @Test
+    @DisplayName("")
+    void getCharacterById() {
+        String nulo = null;
+        Assertions.assertNotEquals();
+    }
+
+    @Test
+    void getLocationById() {
+    }
+
+    @Test
+    void getEpisodeById() {
+    }
+
+    @Test
+    void getAllEpisodes() {
+    }
+
+    @Test
+    void getAllCharacters() {
+    }
+}
